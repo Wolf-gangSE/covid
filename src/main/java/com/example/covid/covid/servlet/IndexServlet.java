f8e6986c7238a4a754575e54839aedbe420a798b
Lucas Henrique de Assis Monteiro <lucasmonte2011@gmail.com>
2021-07-22 02:18:19 -0300

    Primeiro teste de conexão com o PostgreSQL

--- src/main/java/com/example/covid/covid/servlet/IndexServlet.java
+++ src/main/java/com/example/covid/covid/servlet/IndexServlet.java
@@ -1,6 +1,10 @@
 package com.example.covid.covid.servlet;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,7 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet(
 		name = "Index",
-		urlPatterns = {"/api/db"}
+		urlPatterns = {"/db"}
 		)
 
 public class IndexServlet extends HttpServlet{
@@ -18,6 +22,21 @@ public class IndexServlet extends HttpServlet{
   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
     try {
       Class.forName("org.postgresql.Driver");
+      String url = "jdbc:postgresql://localhost:5432/ppsw_covid_webapp";
+      String user = "postgres";
+      String password = "Post*2939";
+      Connection conn = DriverManager.getConnection(url, user, password);
+      Statement listarElementosStn = conn.createStatement();
+      ResultSet rsElementos = listarElementosStn.executeQuery("SELECT * from teste");
+
+      while (rsElementos.next()) {
+        String name = rsElementos.getString("name");
+        resp.getOutputStream().println("[name: " + name + "]");
+      }
+
+      rsElementos.close();
+      listarElementosStn.close();
+      conn.close();
     } catch (Exception e) {
       resp.getOutputStream().print("Ocorreu um erro ao carregar o driver do banco de dados.");
     }
